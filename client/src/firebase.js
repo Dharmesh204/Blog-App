8e24083e178657c29f736d33c798417d6d981518
Dharmesh204 <dharmeshwaikar@yahoo.com>
2024-03-10 17:04:00 +0530

    backend, protected routes, admin routes, and etc.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,22 @@
+/* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  apiKey: "AIzaSyDBV1TPBCs8eC0bYNGMCV_949NyVIl0JGU",
+  authDomain: "blogapp-59cdd.firebaseapp.com",
+  projectId: "blogapp-59cdd",
+  storageBucket: "blogapp-59cdd.appspot.com",
+  messagingSenderId: "42407836558",
+  appId: "1:42407836558:web:a460202ff1639f710742df",
+  measurementId: "G-PG77EWDV29"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,16 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
+};
